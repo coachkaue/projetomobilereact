403355f29a962cc35937827a0895f855b09e96a5
coachkaue <131272124+coachkaue@users.noreply.github.com>
2023-04-19 11:20:24 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,64 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import React, { useState } from "react"
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
 
 export default function App() {
-  
-  const [email, setemail] = useState('')
-  const [nome, setnome] = useState('')
-  const [senha, setsenha] = useState('')
 
-  const cadastro = () => {
-    alert(nome);
-    alert(email);
-    alert(senha);
-  }
-
-
+  const [pessoas, setPessoas] = useState([
+    { nome :'consultoria', key: 0},
+    { nome :'roupas', key: 1},
+    { nome :'suplementação', key: 2},
+    { nome :'manipulados', key: 3},
+  ])
   return (
-    <View style={styles.container}>
-        <StatusBar hidden />
+    <View style={styles.h1}>
+    <FlatList
+      numColumns={2}
+      keyExtractor={(item) => item.key}
+      data={pessoas}
+      renderItem={({ item}) => (
+        <Text style={styles.item}> {item.nome}</Text>
+      )}  
+    />
+    <Text style={{color:'white',padding: 15,marginTop: 15,alignItems: 'center',flexDirection: 'row',justifyContent: 'space-around', textAlign: 'justify', backgroundColor: 'gray',marginRight:9, fontSize:10}}>
+        Informação:  Quais os 3 pilares da saúde?
+Algumas outras necessidades do nosso corpo  chamadas de pilares  estão inteiramente interligadas e também necessitam de atenção para que a vida seja realmente saudável. Dessa forma, podemos resumir os principais pilares da saúde em: exercícios físicos, alimentação, sono e saúde mental.
+        </Text>
+        <Text style={{color:'white',padding: 15,marginTop: 15,alignItems: 'center',flexDirection: 'row',justifyContent: 'space-around', textAlign: 'justify', backgroundColor: 'gray',marginRight:9, fontSize:10}}>
+        Informação: Como se divide a dieta?
+O mais comum é dividido 40:40:20, ou seja, 40% das calorias atribuída à proteína, 40% de carboidrato e 20% de gorduras. A partir daqui, saber quantos gramas de cada macronutriente você precisa é uma questão de aritmética simples. Como exemplo, digamos que seu alvo de ingestão calórica é 2 mil calorias por dia.
+        </Text>
+        <Text style={{color:'white',padding: 15,marginTop: 15,alignItems: 'center',flexDirection: 'row',justifyContent: 'space-around', textAlign: 'justify', backgroundColor: 'gray',marginRight:9, fontSize:10}}>Frequência de treino
 
-        <Image style={{width:300,height:200}} source={require('./assets/2.png')} />
+Informação: Existem pessoas que quando ficam uma semana sem ir para academia querem compensar tudo em um único dia , e isso é um grande erro porque a frequência com que você faz exercícios é mais importante do que a duração dos treinos.
+A frequência de treinamento é o número de sessões realizadas durante um período específico de tempo, sendo grande responsável pelos seus resultados porque precisamos de estímulos contínuos tanto para perder gordura, quanto para hipertrofia. 
+Se você não tem uma frequência, que seja 3,4 ou 5 vezes por semana você pode ter redução dos níveis de força, queda no condicionamento e perda de massa muscular. 
+Não há nenhum problema se você não tem disponibilidade de ir para a academia a semana inteira, o importante é que mesmo indo apenas 3 vezes que você se comprometa com essa frequência. 
+O melhor caminho é alinhar junto com o profissional de educação física qual a melhor estratégia para o seu objetivo e definir qual será a duração de cada treino e a frequência dentro da sua disponibilidade.
+Ser consistente é um desafio, as vezes você tem preguiça de levantar mais cedo, ou a chuva atrapalha ou está cansado demais após o trabalho. A verdade é que sempre vão existir pedras no caminho, mas em vez de focar no problema, você deve focar na solução e fazer o que você se propôs a fazer.</Text>
 
-        <TextInput placeholder='digite seu nome...                  ' style={{width: '100%', height:40,backgroundColor: 'white',borderRadius:20,paddingLeft:10,}} onChangeText={text=>setnome(text)} />
-        <TextInput placeholder='digite seu e-mail...                 ' style={{width: '100%', height:40,backgroundColor: 'white',borderRadius:20,paddingLeft:10,}} onChangeText={text=>setemail(text)} />
-        <TextInput placeholder='digite sua senha...                 ' style={{width: '100%', height:40,backgroundColor: 'white',borderRadius:20,paddingLeft:10,}} onChangeText={text=>setsenha(text)} />
-        
-        <TouchableOpacity style={styles.btncadastro} onPress={()=>cadastro()}>
-          <Text style={{color:'white',textAlign:'center',justifyContent:'center'}}>cadastrar</Text>
-        </TouchableOpacity>
-        
-        
-        
-        
-        
-  
-
-        
+<Text style={{color:'white',padding: 15,marginTop: 15,alignItems: 'center',flexDirection: 'row',justifyContent: 'space-around', textAlign: 'justify', backgroundColor: 'gray',marginRight:9, fontSize:10}}>Informação: Os principais suplementos que podem ser usados para melhorar o desempenho e ganhar massa muscular são: whey protein, creatina, BCAA, glutamina, Caseína, carnitina, L-arginina, Proteínas vegetais </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
+  h1 : {
+    backgroundColor:'white',
+    
+  },
+ 
+  item : {
+    
+    fontSize: 25,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'green',
+    borderRadius:50,
+    marginTop:49,
+    margin:4,
+    flexDirection:"row",
+    alignContent:'center',
+    width: 190,
+    height: 105,
+    textAlign:'center',
+    borderRightWidth:10,
+    paddingRight:10,
     justifyContent: 'center',
-    padding:50
+    
+    
+    
   },
   
-  TextInput: {
-    hidth:'100%',
-    height:40,
-    backgroundColor:'white'
-  },
-  btncadastro:{
-    width:'50%',
-    height:40,
-    backgroundColor:'#27282D'
-  }
-
 });
